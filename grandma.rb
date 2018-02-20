cc3dac07f98c1059b2659904e4e515905c0fde0b
David McGrann <dpmcgrann@gmail.com>
2018-02-20 01:05:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,4 @@
 def speak_to_grandma(phrase)
-  phrase = ""
-  "HUH?! SPEAK UP, SONNY!"
-  if phrase.upcase
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO, PUMPKIN!"
-  end
 end
 
 
