2413c58294e7aa92eaf2bbea9dae27775e161df7
David McGrann <dpmcgrann@gmail.com>
2018-02-20 00:31:45 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,12 @@
-def speak_to_grandma
+def speak_to_grandma(phrase)
+  phrase = ""
+  if "".downcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif "".uppercase
+    "NO, NOT SINCE 1938!"
+  else "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 
 
